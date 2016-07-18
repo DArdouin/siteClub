400485f857193dbe5c413e94118b847090b86f61
DArdouin <da.ardouin@gmail.com>
2016-07-18 23:25:18 +0200

    Petit commit pour mise à jour du .htaccess et de wp-config.php

--- sportMeca/wordpress/wp-config.php
+++ sportMeca/wordpress/wp-config.php
@@ -22,13 +22,13 @@
 define('DB_NAME', 'wordpress');
 
 /** Utilisateur de la base de données MySQL. */
-define('DB_USER', 'root');
+define('DB_USER', 'sml_user');
 
 /** Mot de passe de la base de données MySQL. */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'ZeF6beBhT6zdx4jm');
 
 /** Adresse de l'hébergement MySQL. */
-define('DB_HOST', '127.0.0.1');
+define('DB_HOST', 'localhost');
 
 /** Jeu de caractères à utiliser par la base de données lors de la création des tables. */
 define('DB_CHARSET', 'utf8mb4');
