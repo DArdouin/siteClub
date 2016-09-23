c94f25c962c40f8fd0e0147caa089515fa098fd6
DArdouin <da.ardouin@gmail.com>
2016-09-24 01:05:14 +0200

    Tentative de mise à jour du site

--- sportMeca/wordpress/wp-config.php
+++ sportMeca/wordpress/wp-config.php
@@ -22,13 +22,13 @@
 define('DB_NAME', 'wordpress');
 
 /** Utilisateur de la base de données MySQL. */
-define('DB_USER', 'sml_user');
+define('DB_USER', 'root');
 
 /** Mot de passe de la base de données MySQL. */
-define('DB_PASSWORD', 'ZeF6beBhT6zdx4jm');
+define('DB_PASSWORD', '');
 
 /** Adresse de l'hébergement MySQL. */
-define('DB_HOST', 'localhost');
+define('DB_HOST', '127.0.0.1');
 
 /** Jeu de caractères à utiliser par la base de données lors de la création des tables. */
 define('DB_CHARSET', 'utf8mb4');
